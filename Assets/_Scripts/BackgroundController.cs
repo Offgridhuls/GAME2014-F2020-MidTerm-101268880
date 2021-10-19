ec42dd45d84a80266ec128b268d6ada3f9b3084d
Offgridhuls <55862297+Offgridhuls@users.noreply.github.com>
2021-10-18 20:16:16 -0400

    Initial commit

---
+++ Assets/_Scripts/BackgroundController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    [Header("BackgroundController, " +
+            "Julian Sangiorgio," +
+            "101268880," +
+            "2021-10-18," +
+            "Horizontal Background Movement")]
+    public float horizontalSpeed;
+    public float horizontalBoundary;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Move();
+        _CheckBounds();
+    }
+
+    private void _Reset()
+    {
+        transform.position = new Vector3(horizontalBoundary + 5, 0.0f);
+    }
+
+    private void _Move()
+    {
+        transform.position -= new Vector3((horizontalSpeed) * Time.deltaTime, 0.0f);
+    }
+
+    private void _CheckBounds()
+    {
+        // if the background is lower than the bottom of the screen then reset
+        if (transform.position.x <= -horizontalBoundary)
+        {
+            _Reset();
+        }
+    }
+}
